e86ad099025f1a0d4488c495b06a0d5490078e44
unknown <matheus.duartesilva42@gmail.com>
2023-08-15 18:53:45 -0300

    desenvolvimento da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Nav from './Navigation/Nav.jsx'
 import Products from './Products/Products.jsx'
 import Recommended from './Recommended/Recommended.jsx'
+import Sidebar from './Sidebar/Sidebar.jsx'
 
 const App = () => {
   return (
     <>
+      <Sidebar />
       <Nav />
       <Recommended />
       <Products />
