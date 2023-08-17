d22c78dd1c5d79934d7808b73d93508a752e28d7
unknown <matheus.duartesilva42@gmail.com>
2023-08-17 15:41:15 -0300

    implementação dos filtros

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,70 @@
+import { useState } from 'react'
 import Nav from './Navigation/Nav.jsx'
 import Products from './Products/Products.jsx'
 import Recommended from './Recommended/Recommended.jsx'
 import Sidebar from './Sidebar/Sidebar.jsx'
+import products from './db/data.js'
+import Card from './components/Card.jsx'
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [query, setQuery] = useState('')
+
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
+
+  const filteredItems = products.filter((product) => product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1))
+
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  const handleClick = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  const filteredData = (products, selected, query) => {
+    let filteredProducts = products
+
+    if (query) {
+      filteredProducts = filteredItems
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      )
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={title}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    )
+  }
+
+  const result = filteredData(products, selectedCategory, query)
+
   return (
     <>
-      <Sidebar />
-      <Nav />
-      <Recommended />
-      <Products />
+      <Sidebar handleChange={handleChange} />
+      <Nav query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
     </>
   )
 }
--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,15 +1,43 @@
 import Input from '../../components/Input'
 import './Price.css'
 
-const Price = () => {
+const Price = ({ handleChange }) => {
   return (
     <div className="ml">
       <h2 className="sidebar-title price-title">Price</h2>
 
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value='' name='test' />
+        <span className="checkmark"></span>All
+      </label>
+
+      <Input
+        handleChange={handleChange}
+        value={50}
+        title='$0 - $50'
+        name='test'
+      />
+
+      <Input
+        handleChange={handleChange}
+        value={100}
+        title='$50 - $100'
+        name='test'
+      />
+
+      <Input
+        handleChange={handleChange}
+        value={150}
+        title='$100 - $150'
+        name='test'
+      />
+
+      <Input
+        handleChange={handleChange}
+        value={200}
+        title='Over $150'
+        name='test'
+      />
     </div>
   )
 }
